8e16bc2711ae9061c45dc1ccd62632e3b239f880
ashleydonohoe <ashleyrdonohoe@gmail.com>
2015-08-24 03:10:30 -0400

    fixed indentation

--- media.py
+++ media.py
@@ -1,11 +1,18 @@
 import webbrowser
 
-#class to store movie information
+# class to store movie information
 
 class Movie():
-	def __init__ (self, movie_title, movie_storyline, movie_year, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.year = movie_year
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+
+    """
+    Class used to store the movie title, storyline, release year, poster image
+    and YouTube trailer.
+    """
+
+    def __init__(self, movie_title, movie_storyline, movie_year,
+                 poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.year = movie_year
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
